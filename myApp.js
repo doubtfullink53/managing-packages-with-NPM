9d96149140bc2d2b607049fa78b234d85627043b
Robert  Mathews <83648089+doubtfullink53@users.noreply.github.com>
2022-07-15 13:24:13 -0400

    adjust json API respone logic

--- myApp.js
+++ myApp.js
@@ -25,15 +25,14 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", function (req, res) {
-  if(process.env.MESSAGE_STYLE === "uppercase")  {
-    res.json(
-    {"message": "HELLO JSON"}
-    )
-  }else { 
-    res.json(
-    {"message": "hello json"}
-    )
-  }
+
+   var jsonResponse = {"message": "Hello json"};
+   if(process.env.MESSAGE_STYLE === "uppercase") {
+        jsonResponse.message = jsonResponse.message.toUpperCase();
+   }
+    res.json(jsonResponse);
+
+
 })
 
 
