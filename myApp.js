c2d4fee1b3ab60753b016b8820d2b6c42032a8ae
Robert  Mathews <83648089+doubtfullink53@users.noreply.github.com>
2022-07-15 13:08:06 -0400

    Use the .env File uppercase hello json

--- myApp.js
+++ myApp.js
@@ -1,12 +1,12 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 
 
 
 
-
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
@@ -16,15 +16,25 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 
-app.get("/json", (req, res) => {   
-    res.json({
-        message: "Hello json"
-    });
-});
+// app.get("/json", (req, res) => {   
+//     res.json({
+//         message: "Hello json"
+//     });
+// });
 
 
 
-
+app.get("/json", function (req, res) {
+  if(process.env.MESSAGE_STYLE === "uppercase")  {
+    res.json(
+    {"message": "HELLO JSON"}
+    )
+  }else { 
+    res.json(
+    {"message": "hello json"}
+    )
+  }
+})
 
 
 
